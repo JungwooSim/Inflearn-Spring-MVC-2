e74205c77edb7a2bd3c8e9a84afe3b3b1c35f239
JungwooSim <cscd053@gmail.com>
2021-07-18 22:35:35 +0900

    API 에외 처리 / 스프링이 제공하는 ExceptionResolver1

---
+++ exception/src/main/java/hello/exception/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package hello.exception.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// messages.properties 메시지를 직접 사용도 가능
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
+public class BadRequestException extends RuntimeException{
+}
